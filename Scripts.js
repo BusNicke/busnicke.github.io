f2186facd1dc9703d4ca3c0d626fedbf439e5169
gustav_akman <gustav_akman@hotmail.com>
2016-06-03 09:11:47 +0200

    fixed middlemouse click

--- Scripts.js
+++ Scripts.js
@@ -5,9 +5,12 @@ function box(flag, mine, clicked, neighborsWithMine){
     this.neighborsWithMine = neighborsWithMine;
 };
 
-var numberOfMines;
+var timer = 0;
+var numberOfMines = 0;
 var gameBoard;
 var gameBoardWidthAndHeight;
+var gameOver = false;
+var numberOfFlags = 0;
 
 $(document).ready(function(){
     $("#startGame").click(function(){
@@ -28,12 +31,14 @@ $(document).ready(function(){
         if(checkNumberOfMines >= widthInput * heightInput)
             checkNumberOfMines = (widthInput * heightInput)-1;
         numberOfMines = checkNumberOfMines;
-        $("#startGame").hide();
+        $(".setUpGame").hide();
         CreateGameBoard(gameBoardWidthAndHeight.width, gameBoardWidthAndHeight.height);
     });
 });
 
 function CreateGameBoard(height, width){
+    document.oncontextmenu = function() {return false;};
+    
     gameBoard = new Array(width)
     for (i=0; i < width; i++)
         gameBoard[i] = new Array(height);
@@ -41,24 +46,156 @@ function CreateGameBoard(height, width){
     for(var i = 0; i < width; i++){
         $("#canvas").append("<div class='row" + i + "'></div>")
         for(var j = 0; j < height; j++){
-            $(".row"+ i).append("<div class='box' id='"+ i + "" + j +"' onClick='CheckForMines(" + i + ", " + j + ")'></div>");           
+            $(".row"+ i).append("<div class='box' id='"+ i + "t" + j +"' onClick='CheckForMines(" + i + ", " + j + ")'></div>");           
             gameBoard[i][j] = new box(flag = false, mine = false, clicked = false, neighborsWithMine = 0);
         }
         $("#canvas").append("<br />")
     }
     AssignMines();
     CheckNeighborsWithMines();
-    
+    UpdateMinesLeft();
     ActivateFlagPressing();
+    StartTimer();
+};
+
+function StartTimer(){
+    var doUpdate = function() {
+    $('#countdown').each(function() {
+      if (gameOver === false) {
+        timer++;
+        var minutes = Math.floor(timer / 60);
+        var seconds = timer % 60;
+        if(seconds < 10){
+            seconds = 0 + seconds.toString();
+        }
+        $(this).html("Time:" + minutes + ":" + seconds);
+      }
+    });
+  };
+
+  setInterval(doUpdate, 1000);
 };
 
 function ActivateFlagPressing(){
     $(".box").mousedown(function(ev){
+        
+        var id = $(this).prop("id").split("t");
         if(ev.which == 3)
         {
-            $(this).addClass("flag");
+            if(gameBoard[id[0]][id[1]].flag === true){
+                $(this).removeClass("fa fa-flag flag");
+                gameBoard[id[0]][id[1]].flag = false;
+                numberOfFlags--;
+                UpdateMinesLeft();
+            }
+            else{
+                if(gameBoard[id[0]][id[1]].clicked === false){
+                    gameBoard[id[0]][id[1]].flag = true;
+                    $(this).addClass("fa fa-flag flag");
+                    numberOfFlags++;
+                    UpdateMinesLeft(); 
+                }
+            } 
+        }
+        if(ev.which == 2){
+            //alert("middlemouse");
+            CheckNeighborsForFlags(id[0], id[1]);
         }
     });
+};
+
+function CheckNeighborsForFlags(i, j){
+    i = parseInt(i);
+    j = parseInt(j);
+    var neighborsWithFlags = 0;
+    if(i-1 >= 0 && j-1 >= 0){
+        if(gameBoard[i-1][j-1].flag === true)
+            neighborsWithFlags++;
+    }
+        
+    if(i-1 >= 0){
+        if(gameBoard[i-1][j].flag === true)
+            neighborsWithFlags++;
+    }
+        
+    if(i-1 >= 0 && j+1 < gameBoardWidthAndHeight.width){
+        if(gameBoard[i-1][j+1].flag === true)
+            neighborsWithFlags++;
+    }
+        
+    if(j-1 >= 0){
+        if(gameBoard[i][j-1].flag === true)
+            neighborsWithFlags++;
+    }
+        
+    if(j+1 < gameBoardWidthAndHeight.width){
+        if(gameBoard[i][j+1].flag === true)
+            neighborsWithFlags++;
+    }
+        
+    if(i+1 < gameBoardWidthAndHeight.height && j-1 >= 0){
+        if(gameBoard[i+1][j-1].flag === true)
+            neighborsWithFlags++;
+    }
+        
+    if(i+1 < gameBoardWidthAndHeight.height){
+        if(gameBoard[i+1][j].flag === true)
+            neighborsWithFlags++;
+    }
+        
+    if(i+1 < gameBoardWidthAndHeight.height && j+1 < gameBoardWidthAndHeight.width){
+        if(gameBoard[i+1][j+1].flag === true)
+            neighborsWithFlags++;
+    }
+    if(neighborsWithFlags === gameBoard[i][j].neighborsWithMine){
+        ClickAllNeighbors(i, j);
+    }
+};
+
+function ClickAllNeighbors(i, j){
+    if(i-1 >= 0 && j-1 >= 0){
+        if(gameBoard[i-1][j-1].flag === false && gameBoard[i-1][j-1].clicked === false)
+            CheckForMines(i-1,j-1);
+    }
+        
+    if(i-1 >= 0){
+        if(gameBoard[i-1][j].flag === false && gameBoard[i-1][j].clicked === false)
+            CheckForMines(i-1,j);
+    }
+        
+    if(i-1 >= 0 && j+1 < gameBoardWidthAndHeight.height){
+        if(gameBoard[i-1][j+1].flag === false && gameBoard[i-1][j+1].clicked === false)
+            CheckForMines(i-1,j+1);
+    }
+        
+    if(j-1 >= 0){
+        if(gameBoard[i][j-1].flag === false && gameBoard[i][j-1].clicked === false)
+            CheckForMines(i,j-1);
+    }
+        
+    if(j+1 < gameBoardWidthAndHeight.height){
+        if(gameBoard[i][j+1].flag === false && gameBoard[i][j+1].clicked === false)
+            CheckForMines(i,j+1);
+    }
+        
+    if(i+1 < gameBoardWidthAndHeight.width && j-1 >= 0){
+        if(gameBoard[i+1][j-1].flag === false && gameBoard[i+1][j-1].clicked === false)
+            CheckForMines(i+1,j-1);
+    }
+        
+    if(i+1 < gameBoardWidthAndHeight.width){
+        if(gameBoard[i+1][j].flag === false && gameBoard[i+1][j].clicked === false)
+            CheckForMines(i+1,j);
+    }
+        
+    if(i+1 < gameBoardWidthAndHeight.width && j+1 < gameBoardWidthAndHeight.height){
+        if(gameBoard[i+1][j+1].flag === false && gameBoard[i+1][j+1].clicked === false)
+            CheckForMines(i+1,j+1);
+    }
+};
+
+function UpdateMinesLeft(){
+    $("#minesLeft").text("Mines left: " + (numberOfMines - numberOfFlags));
 };
 
 function AssignMines(){
@@ -85,7 +222,7 @@ function CheckNeighborsWithMines(){
                     gameBoard[i][j].neighborsWithMine++;
             }
                 
-            if(i-1 >= 0 && j+1 < gameBoardWidthAndHeight.height){
+            if(i-1 >= 0 && j+1 < gameBoardWidthAndHeight.width){
                 if(gameBoard[i-1][j+1].mine === true)
                     gameBoard[i][j].neighborsWithMine++;
             }
@@ -95,22 +232,22 @@ function CheckNeighborsWithMines(){
                     gameBoard[i][j].neighborsWithMine++;
             }
                 
-            if(j+1 < gameBoardWidthAndHeight.height){
+            if(j+1 < gameBoardWidthAndHeight.width){
                 if(gameBoard[i][j+1].mine === true)
                     gameBoard[i][j].neighborsWithMine++;
             }
                 
-            if(i+1 < gameBoardWidthAndHeight.width && j-1 >= 0){
+            if(i+1 < gameBoardWidthAndHeight.height && j-1 >= 0){
                 if(gameBoard[i+1][j-1].mine === true)
                     gameBoard[i][j].neighborsWithMine++;
             }
                 
-            if(i+1 < gameBoardWidthAndHeight.width){
+            if(i+1 < gameBoardWidthAndHeight.height){
                 if(gameBoard[i+1][j].mine === true)
                     gameBoard[i][j].neighborsWithMine++;
             }
                 
-            if(i+1 < gameBoardWidthAndHeight.width && j+1 < gameBoardWidthAndHeight.height){
+            if(i+1 < gameBoardWidthAndHeight.height && j+1 < gameBoardWidthAndHeight.width){
                 if(gameBoard[i+1][j+1].mine === true)
                     gameBoard[i][j].neighborsWithMine++;
             }
@@ -119,55 +256,36 @@ function CheckNeighborsWithMines(){
 };
 
 function CheckForMines(x, y){
-    
-    gameBoard[x][y].clicked = true;
-    
-    if(gameBoard[x][y].mine === true){
-        $("#" + x +""+ y).addClass("clickedMine fa fa-bomb");
-        revealAllMines();
+    if(gameOver === false){
+        if(gameBoard[x][y].flag === false){
+            gameBoard[x][y].clicked = true;
+            if(gameBoard[x][y].mine === true){
+                $("#" + x +"t"+ y).addClass("clickedMine fa fa-bomb");
+                revealAllMines();
+                gameOver = true;
+            }
+            
+            else if(gameBoard[x][y].neighborsWithMine === 0){
+                checkIfNeighborsIsEmpty(x, y);
+                $("#" + x +"t"+ y).addClass("clicked");
+            }
+            
+            else{
+                $("#" + x +"t"+ y).text(gameBoard[x][y].neighborsWithMine);
+                $("#" + x +"t"+ y).addClass("numberBox"+ gameBoard[x][y].neighborsWithMine + " clicked"); 
+            }
+            
+            //$("#" + x +"t"+ y).prop("disabled", true);
+            checkWinningCondition();
+        }
     }
-    
-    else if(gameBoard[x][y].neighborsWithMine === 0){
-        checkIfNeighborsIsEmpty(x, y);
-        $("#" + x +""+ y).addClass("clicked");
-    }
-    
-    else{
-        $("#" + x +""+ y).text(gameBoard[x][y].neighborsWithMine);
-        if(gameBoard[x][y].neighborsWithMine === 1)
-            $("#" + x +""+ y).addClass("numberBoxOne clicked"); 
-            
-        if(gameBoard[x][y].neighborsWithMine === 2)
-            $("#" + x +""+ y).addClass("numberBoxTwo clicked"); 
-            
-        if(gameBoard[x][y].neighborsWithMine === 3)
-            $("#" + x +""+ y).addClass("numberBoxThree clicked"); 
-            
-        if(gameBoard[x][y].neighborsWithMine === 4)
-            $("#" + x +""+ y).addClass("numberBoxFour clicked"); 
-            
-        if(gameBoard[x][y].neighborsWithMine === 5)
-            $("#" + x +""+ y).addClass("numberBoxFive clicked"); 
-            
-        if(gameBoard[x][y].neighborsWithMine === 6)
-            $("#" + x +""+ y).addClass("numberBoxSix clicked"); 
-            
-        if(gameBoard[x][y].neighborsWithMine === 7)
-            $("#" + x +""+ y).addClass("numberBoxSeven clicked"); 
-            
-        if(gameBoard[x][y].neighborsWithMine === 8)
-            $("#" + x +""+ y).addClass("numberBoxEight clicked"); 
-    }
-    
-    $("#" + x +""+ y).prop("disabled", true);
-    
 };
 
 function revealAllMines(){
     for(var i = 0; i < gameBoardWidthAndHeight.width; i++){
         for(var j = 0; j < gameBoardWidthAndHeight.height; j++){
             if(gameBoard[i][j].mine === true)
-                $("#" + i +""+ j).addClass("clicked mine fa fa-bomb");   
+                $("#" + i +"t"+ j).addClass("clicked mine fa fa-bomb");   
         }
     }    
 };
@@ -191,5 +309,20 @@ function checkIfNeighborsIsEmpty(x, y){
     if(x+1 < gameBoardWidthAndHeight.width){
         if(gameBoard[x+1][y].mine === false && gameBoard[x+1][y].clicked === false)
             CheckForMines(x+1, y);
+    }
+};
+
+function checkWinningCondition(){
+    var win = true;
+    for(var i = 0; i < gameBoardWidthAndHeight.width; i++){
+        for(var j = 0; j < gameBoardWidthAndHeight.height; j++){
+            if(gameBoard[i][j].mine === false && gameBoard[i][j].clicked === false)
+                win = false;
+        }
+    }
+    
+    if(win === true){
+        $("#win").text("You WON!");
+        gameOver = true;
     }
 };
